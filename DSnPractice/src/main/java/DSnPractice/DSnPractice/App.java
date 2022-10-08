a9d513989039fb6000424c263802bb2803750a9e
“mohdmyyusuf@gmail.com” <mohdmyyusuf@gmail.com>
2022-10-09 02:05:05 +0530

    First commit

---
+++ DSnPractice/src/main/java/DSnPractice/DSnPractice/App.java
@@ -0,0 +1,13 @@
+package DSnPractice.DSnPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ DSnPractice/src/test/java/TQOne.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class TQOne {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int one = 'a'/'a'; 
+		String s = ".........";
+		for (int i = one; i < (s.length()*s.length()); i++) {
+			System.out.print(i);
+		}
+		
+	}
+
+}
